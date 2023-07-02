d0f3b250f5ab7120025828bcc64ae70da253f22b
Muhammad Khattab <msskzx@gmail.com>
2023-07-02 18:59:32 +0200

    ex2 - dataset and 3D CNN

--- E2/exercise_2/simple_nn.py
+++ E2/exercise_2/simple_nn.py
@@ -2,45 +2,64 @@ from pathlib import Path
 
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from exercise_2.util.toy_data import generate_toy_data
 
 
 class SimpleDataset(torch.utils.data.Dataset):
     # TODO: Implement __init__, __getitem__, __len__
-    def __init__(self):
-        pass
+    def __init__(self, phase='train'):
+        super().__init__()
+        assert phase in ['train', 'val']
+        TRAIN_NUM_SAMPLES = 4096
+        VAL_NUM_SAMPLES = 1024
+        self.input_data, self.target_labels = generate_toy_data(num_samples=TRAIN_NUM_SAMPLES if phase == 'train' else VAL_NUM_SAMPLES)
 
     def __getitem__(self, idx):
-        pass
+        return self.input_data[idx][np.newaxis, :], self.target_labels[idx]
 
     def __len__(self):
-        pass
+        return len(self.target_labels)
 
 
 class SimpleModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = torch.nn.Conv3d(in_channels=1, out_channels=4, kernel_size=4, stride=3, padding=1)
-        self.bn1 = torch.nn.BatchNorm3d(4)
-        # TODO: Add conv2 and conv3 with the same parameters as conv1; also, add bn2 and bn3
+        self.features = nn.Sequential(
+            nn.Conv3d(in_channels=1, out_channels=4, kernel_size=4, stride=3, padding=1),
+            nn.BatchNorm3d(4),
+            nn.ReLU(),
+            # TODO: Add conv2 and conv3 with the same parameters as conv1; also, add bn2 and bn3,
+            nn.Conv3d(in_channels=4, out_channels=8, kernel_size=4, stride=3, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            nn.Conv3d(in_channels=8, out_channels=16, kernel_size=4, stride=3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU()
+        )
 
         # TODO: Add Linear layer for classification which reduces the number of features from 16 to 2
-        # TODO: Add a ReLU
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=16, out_features=2),
+            nn.ReLU()
+        )
 
     def forward(self, x):
-        x = self.relu(self.bn1(self.conv1(x)))
         # TODO: Move tensor through layers 2 and 3
+        x = self.features(x)
         # TODO: Apply the classification layer. Use .view() to reshape the output of layer 3 into the correct format
-
+        x = x.view(-1, 16)
+        x = self.classifier(x)
         return x
 
 
 def train(model, train_dataloader, val_dataloader, device, config):
     # TODO Declare Loss function; Use CrossEntropyLoss
-    loss_criterion = None
+    loss_criterion = nn.CrossEntropyLoss().to(device)
 
     # TODO Declare optimizer; Use ADAM with learning rate from config['learning_rate']
-    optimizer = None
+    optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
 
     # Set model to train, important if your network has e.g. dropout or batchnorm layers
     model.train()
@@ -55,17 +74,19 @@ def train(model, train_dataloader, val_dataloader, device, config):
         for i, batch in enumerate(train_dataloader):
             input_data, target_labels = batch
             # TODO Move input_data and target_labels to device
+            input_data = input_data.to(device)
+            target_labels = target_labels.to(device)
 
             # This is where the actual training happens:
-            # 1 Zero out gradients from last iteration
+            # 1. Zero out gradients from last iteration
             optimizer.zero_grad()
-            # 2 Perform forward pass
+            # 2. Perform forward pass
             prediction = model(input_data)
-            # 3 Calculate loss
+            # 3. Calculate loss
             loss = loss_criterion(prediction, target_labels)
-            # 4 Compute gradients
+            # 4. Compute gradients
             loss.backward()
-            # 5 Adjust weights using the optimizer
+            # 5. Adjust weights using the optimizer
             optimizer.step()
 
             # Loss logging
@@ -87,6 +108,8 @@ def train(model, train_dataloader, val_dataloader, device, config):
                 for batch_val in val_dataloader:
                     input_data, target_labels = batch_val
                     # TODO Move input_data and target_labels to device
+                    input_data = input_data.to(device)
+                    target_labels = target_labels.to(device)
 
                     with torch.no_grad():
                         prediction = model(input_data)
@@ -120,7 +143,7 @@ def main(config):
         print('Using CPU')
 
     # Create Dataloaders
-    train_dataset = None  # TODO Instantiate Dataset in train split
+    train_dataset = SimpleDataset(phase='train')  # TODO Instantiate Dataset in train split
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,   # Datasets return data one sample at a time; Dataloaders use them and aggregate samples into batches
         batch_size=config['batch_size'],   # The size of batches is defined here
@@ -129,7 +152,7 @@ def main(config):
         pin_memory=True  # This is an implementation detail to speed up data uploading to the GPU
     )
 
-    val_dataset = None  # TODO Instantiate Dataset in val split
+    val_dataset = SimpleDataset(phase='val')  # TODO Instantiate Dataset in val split
     val_dataloader = torch.utils.data.DataLoader(
         val_dataset,     # Datasets return data one sample at a time; Dataloaders use them and aggregate samples into batches
         batch_size=config['batch_size'],   # The size of batches is defined here
@@ -139,7 +162,7 @@ def main(config):
     )
 
     # TODO Instantiate model and move to device
-    model = None
+    model = SimpleModel().to(device)
 
     # Create folder for saving checkpoints
     Path(f'exercise_2/runs/{config["experiment_name"]}').mkdir(exist_ok=True, parents=True)
--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 # Machine Learning for 3D Geometry
 
 ## Outline
+---
 - ### Surface Representation and Alignment
 - ### Shape Classification and Segmentation
 - ### 3D Shape Reconstruction
 - ### References
----
+
 ## Surface Representation and Alignment
+---
+
 ### Signed Distance Fields
+---
 Given a point calculate the SDF function for:
 - sphere
 - torus
 - atom: combines sphere and torus SDF function
 - SDF grids: build an SDF grid that shows the implicit representation in a cube of size 1x1x1
 - Visualization of `.ply` objects in `MeshLab`
+
 ### Conversion from SDF Grids to Occupancy Grids
+---
+
 ### Conversion From SDF to Triangle Meshes
+---
 - Marching Cubes
 
 Sphere            |  Torus
@@ -28,26 +36,30 @@ Atom            |  MLP
 
 - Vertex Interpolation
 - Export triangle meshes as `Wavefront OJB`
+
 ### Conversion from Triangle Meshes to Point Clouds
+---
 - Sample n points using barycentric coordinates for each triangle
 - Export point cloud as `Wavefront OBJ`
 
 ![sphere](./E1/images/atom-point-cloud.png)
 
 ### Rigid Shape Alignment with Procrustes
+---
 - align two shapes given near-perfect point correspondences using procrustes
 
 Before            |  After
 :-------------------------:|:-------------------------:
 ![before](./E1/images/alignment-before.png) | ![after](./E1/images/alignment-after.png)
 
----
 ## Shape Classification and Segmentation
 ---
+
 ## 3D Shape Reconstruction
 ---
-## References
 
+## References
+---
 [1] Park, Jeong Joon, et al. "Deepsdf: Learning continuous signed distance functions for shape representation." Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition. 2019
 
 [2] Mescheder, Lars, et al. "Occupancy networks: Learning 3d reconstruction in function space." Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition. 2019.
